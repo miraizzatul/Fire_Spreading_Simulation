46ccd4b089d06461cf3fcc53a4f0a9317b37c9d3
Mira <nurizzatulamiramuzafar@gmail.com>
2026-04-08 02:17:14 +0800

    Removed Intermediate, Binaries and Saved folders from repo

--- Mira_Izzatul/Intermediate/Build/Win64/x64/Mira_IzzatulEditor/Development/Core/SharedPCH.Core.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Users/User/Documents/Unreal Projects/FireSpreading_Simulation/Mira_Izzatul/Intermediate/Build/Win64/x64/Mira_IzzatulEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- Mira_Izzatul/Intermediate/Build/Win64/x64/Mira_IzzatulEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.38.33145
-#include "C:/Users/User/Documents/Unreal Projects/FireSpreading_Simulation/Mira_Izzatul/Intermediate/Build/Win64/x64/Mira_IzzatulEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h"
